7dc45305a8fc1f47bdd5e1896350e1e48dcbcdfe
Dinh Tran <dinh.tran@retailfoundation.de>
2023-09-12 12:46:43 +0700

    Fix leak access token

--- terraform/aws/ec2/main.tf
+++ terraform/aws/ec2/main.tf
@@ -13,9 +13,9 @@ terraform {
 provider "aws" {
   region   = "ap-southeast-1"
 
-  access_key = "ASIAZJT35OB4SXTIJQQ4"
-  secret_key = "b+YWNxM3VXZrVCovRprwOl35n96yX7IpkGV04eW+"
-  token ="IQoJb3JpZ2luX2VjEDcaDmFwLXNvdXRoZWFzdC0xIkcwRQIhAPv8ZpPGtp75SWWL0az5APsIVq5sib8tondlTh5rBKx/AiAW/YTaaaTIXhOK8qBoA5DciJCoRfOvbAjbqtukj9SaOyr0AQjw//////////8BEAAaDDYzOTEzNjI2NDMxMyIMZBejOiayOCyQ7TsPKsgBt697u2OTh2tUAYCrv8NhwO5pu1zUtb/InvvrTiw5GQe8A5gimQAWlNZwsO6IsZ/kiCNYd/7EqsnNMBHi1lk/NOyG9oEQvi4Jurp/5C4RNHBrNsHeIiTYS0GKyFDgPCxKN9USLjAknbFa2RyvD539nL6cTiY5F4Oi01B1Hf5GZop6EhA7P87ExLZf8sHJeLRMBmIp/CxVRIs0EoR7llXiZSSRJEMv5rB7RkcA95emg5/8I1XWpaBPH7XM0x3+awemVKDJGSERzWwwtZOTpwY6mAHhdNRTph3+kF7W7RCER7lee6BnxSozFLYgJ+WsGtNDqbd31LRMZ9KpQ+uyPfu9zDj/MmEQuzflcAWvQSEs8ZAHUW6U4UDirYqWPnAhXUsmVv7EHpmJ2xo+Uz6VQJ8dbKS032T1DnkgTi/Nc4vzBnZXozRnB7vJ1p/cZcu1HHvwBxHUOkEfQbtOw8UfQvIcizzFQMY+0pDvNg=="
+  access_key = ""
+  secret_key = ""
+  token =""
 }
 
 resource "aws_instance" "dinhvt_bastion" {
